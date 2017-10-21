818f8a3bef2b3e1029aa7b10d2f09bad72efab3d
roderickzxy <roderickzxy@qq.com>
2017-10-22 00:19:28 +0800

    添加websocket功能代码，以及简陋的页面代码

--- src/main/java/com/rode/listener/WebContextListener.java
+++ src/main/java/com/rode/listener/WebContextListener.java
@@ -1,19 +1,19 @@
-package com.rode.test;
+package com.rode.listener;
 
 import java.net.UnknownHostException;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 import org.java_websocket.WebSocketImpl;
 
 import com.rode.websocket.WebSocket;
 
-/**
- * The test class
- * 
- * @author Roderick
- */
-public class TestMain {
-	public static void main(String[] args) throws InterruptedException {
-		System.out.println("开始启动websocket");
+public class WebContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    	System.out.println("开始启动websocket");
 		WebSocketImpl.DEBUG = false;
 		int port = 8888; // 端口随便设置，只要不跟现有端口重复就可以
 		WebSocket s = null;
@@ -25,5 +25,11 @@ public class TestMain {
 		}
 		s.start();
 		System.out.println("启动websocket成功！");
-	}
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("===========被销毁=============");
+    }
+
 }
---
+++ src/main/java/com/rode/servlet/MessageServlet.java
@@ -0,0 +1,27 @@
+package com.rode.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MessageServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { 
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("content" + req.getParameter("content"));
+		req.getSession().setAttribute("content", req.getParameter("content"));
+		req.getRequestDispatcher("/index.jsp").forward(req, resp);
+	}
+}
